4465a15e045ce2979faf728ed73df806de6a405a
jhurtados <juaneshs2014@gmail.com>
2024-10-23 23:44:11 -0500

    aja

--- src/app/noUserPages/routes-no-user-pages.routing.ts
+++ src/app/noUserPages/routes-no-user-pages.routing.ts
@@ -2,24 +2,23 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
-// import { noAuthGuard } from '../auth/no-auth.guard';
+import { noAuthGuard } from '../auth/no-auth.guard';
 
 const routes: Routes = [
   {
     path: 'login',
     component: LoginComponent,
-    // canActivate: [noAuthGuard],
-    // Agregamos este console.log para depurar
+    canActivate: [noAuthGuard],
     data: {
-      debug: () => console.log('Login Route Reached EEEEEEEEEE')  // Esto se verá en la consola si la ruta es alcanzada
+      debug: () => console.log('Login Route Reached EEEEEEEEEE')
     }
   },
   {
     path: 'register',
     component: RegisterComponent,
-    // canActivate: [noAuthGuard],
+    canActivate: [noAuthGuard],
     data: {
-      debug: () => console.log('Register Route Reached')  // Depuración en la ruta de registro
+      debug: () => console.log('Register Route Reached')
     }
   },
 ];
